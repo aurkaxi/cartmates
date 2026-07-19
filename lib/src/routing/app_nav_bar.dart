69579252ae81f182598d4c6ba2ac4c74496c8f31
aurkaxi <abdullahalmuaz15@gmail.com>
2026-07-19 22:34:24 +0600

    refactor: use material3 navbar instead of custom

--- lib/src/routing/app_nav_bar.dart
+++ lib/src/routing/app_nav_bar.dart
@@ -8,36 +8,36 @@ class AppNavBar extends StatelessWidget {
 
   final String currentRoute;
 
-  static const _tabs = [
-    _TabItem(
+  static const List<_NavTab> _tabs = [
+    _NavTab(
       route: AppRoutes.create,
       label: 'Create',
-      filledIcon: Icons.add_circle,
-      outlinedIcon: Icons.add_circle_outline,
+      icon: Icons.add_circle_outline,
+      selectedIcon: Icons.add_circle,
     ),
-    _TabItem(
+    _NavTab(
       route: AppRoutes.cart,
       label: 'Cart',
-      filledIcon: Icons.shopping_cart,
-      outlinedIcon: Icons.shopping_cart_outlined,
+      icon: Icons.shopping_cart_outlined,
+      selectedIcon: Icons.shopping_cart,
     ),
-    _TabItem(
+    _NavTab(
       route: AppRoutes.deals,
       label: 'Deals',
-      filledIcon: Icons.local_offer,
-      outlinedIcon: Icons.local_offer_outlined,
+      icon: Icons.local_offer_outlined,
+      selectedIcon: Icons.local_offer,
     ),
-    _TabItem(
+    _NavTab(
       route: AppRoutes.campaigns,
       label: 'Campaigns',
-      filledIcon: Icons.campaign,
-      outlinedIcon: Icons.campaign_outlined,
+      icon: Icons.campaign_outlined,
+      selectedIcon: Icons.campaign,
     ),
-    _TabItem(
+    _NavTab(
       route: AppRoutes.profile,
       label: 'Profile',
-      filledIcon: Icons.person,
-      outlinedIcon: Icons.person_outline,
+      icon: Icons.person_outline,
+      selectedIcon: Icons.person,
     ),
   ];
 
@@ -56,90 +56,36 @@ class AppNavBar extends StatelessWidget {
 
     return SafeArea(
       top: false,
-      child: Container(
-        height:
-            kBottomNavigationBarHeight + MediaQuery.paddingOf(context).bottom,
-        decoration: BoxDecoration(
-          color: cs.surface,
-          border: Border(top: BorderSide(color: cs.outlineVariant)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: List.generate(_tabs.length, (index) {
-            final tab = _tabs[index];
-            final isActive = index == _currentIndex;
-            return _NavItem(
-              onTap: () => context.go(tab.route),
-              isActive: isActive,
-              filledIcon: tab.filledIcon,
-              outlinedIcon: tab.outlinedIcon,
-              cs: cs,
-              label: tab.label,
-            );
-          }),
-        ),
+      child: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) => context.go(_tabs[index].route),
+        height: kBottomNavigationBarHeight,
+        indicatorColor: cs.primaryContainer,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        destinations: _tabs
+            .map(
+              (tab) => NavigationDestination(
+                icon: Icon(tab.icon),
+                selectedIcon: Icon(tab.selectedIcon),
+                label: tab.label,
+              ),
+            )
+            .toList(),
       ),
     );
   }
 }
 
-class _TabItem {
-  const _TabItem({
+class _NavTab {
+  const _NavTab({
     required this.route,
     required this.label,
-    required this.filledIcon,
-    required this.outlinedIcon,
+    required this.icon,
+    required this.selectedIcon,
   });
 
   final String route;
   final String label;
-  final IconData filledIcon;
-  final IconData outlinedIcon;
-}
-
-class _NavItem extends StatelessWidget {
-  const _NavItem({
-    required this.onTap,
-    required this.isActive,
-    required this.filledIcon,
-    required this.outlinedIcon,
-    required this.cs,
-    required this.label,
-  });
-
-  final VoidCallback onTap;
-  final bool isActive;
-  final IconData filledIcon;
-  final IconData outlinedIcon;
-  final ColorScheme cs;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        onTap: onTap,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              isActive ? filledIcon : outlinedIcon,
-              size: 24.sp,
-              color: isActive ? cs.primary : cs.onSurfaceVariant,
-            ),
-            SizedBox(height: 2.h),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 11.sp,
-                fontWeight: isActive ? FontWeight.w600 : FontWeight.w400,
-                color: isActive ? cs.primary : cs.onSurfaceVariant,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  final IconData icon;
+  final IconData selectedIcon;
 }
